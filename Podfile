101ddfbbcbf9786fce9fe4f7771b5fa216e72c59
Somesh Vyas <somesh.vyas@bookmyshow.com>
2015-12-14 22:13:11 +0530

    successfully integrated Kolada View

---
+++ Podfile
@@ -0,0 +1,19 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '8.0'
+# Uncomment this line if you're using Swift
+use_frameworks!
+
+target 'SlideoutMenu' do
+	source 'https://github.com/CocoaPods/Specs.git'
+	platform :ios, '9.1'
+	pod 'pop', '~>1.0'
+end
+
+target 'SlideoutMenuTests' do
+
+end
+
+target 'SlideoutMenuUITests' do
+
+end
+
---
+++ SlideoutMenu/ExampleOverlayView.swift
@@ -0,0 +1,36 @@
+//
+//  ExampleOverlayView.swift
+//  SlideoutMenu
+//
+//  Created by Somesh Vyas on 12/12/15.
+//  Copyright © 2015 Somesh Vyas. All rights reserved.
+//
+
+private let overlayRightImageName = "yesOverlayImage"
+private let overlayLeftImageName = "noOverlayImage"
+
+class ExampleOverlayView: OverlayView {
+    @IBOutlet lazy var overlayImageView: UIImageView! = {
+        [unowned self] in
+        
+        var imageView = UIImageView(frame: self.bounds)
+        self.addSubview(imageView)
+        
+        return imageView
+        }()
+    
+    override var overlayState:OverlayMode  {
+        didSet {
+            switch overlayState {
+            case .Left :
+                overlayImageView.image = UIImage(named: overlayLeftImageName)
+            case .Right :
+                overlayImageView.image = UIImage(named: overlayRightImageName)
+            default:
+                overlayImageView.image = nil
+            }
+            
+        }
+    }
+    
+}
